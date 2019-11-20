88212eb3eed6d4c938f08270fa7a4c638cd681ca
Kevin Wu <peiyuwu9@gmail.com>
2019-11-20 17:45:55 -0500

    Update mongodb configuration

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+mongoose.connect( MONGODB_URI, { useNewUrlParser: true });
 
 // 0. HTML route to home page
 app.get("/", function (req, res) {
